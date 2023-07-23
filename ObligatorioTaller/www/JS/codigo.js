b1bd9cee213f8f87afd0f7ddc226d34ca12e3e8f
RodriguezAndres01 <andres.rodriguez@pedidosya.com>
2023-07-23 11:29:37 -0300

    Algunas constantes y manejo de errores de la api
    Se creó algunas constantes para manejar el url base del API y el URL de los endpoints.
    Se centralizó el manejo de errores desde la api.
    Se implementó el uso de arrow functions

--- ObligatorioTaller/www/JS/codigo.js
+++ ObligatorioTaller/www/JS/codigo.js
@@ -1,5 +1,14 @@
-//let hayUsuarioLogueado = false;
+// Constantes para las URL de la API
+const API_BASE_URL = "https://censo.develotion.com/";
+const API_LOGIN_ENDPOINT = API_BASE_URL + "login.php";
+const API_USUARIOS_ENDPOINT = API_BASE_URL + "usuarios.php";
+
+// Variable para almacenar el token del usuario
 let token;
+
+let hayUsuarioLogueado = false;
+localStorage.setItem("hayUsuarioLogueado", hayUsuarioLogueado)
+
 console.log(localStorage.getItem("hayUsuarioLogueado"));
 if(localStorage.getItem("hayUsuarioLogueado")){
     Inicio(true);
@@ -75,19 +84,19 @@ function MostrarOcultarDivs() {
             break;
     }
 }
+// Función para iniciar sesión
 function IniciarSesion() {
     let nombreUsuario = document.querySelector("#nombreUsuario").value;
     let password = document.querySelector("#password").value;
     document.querySelector("#errorMessage").innerHTML = "";
-
- 
 
     try {
         if (nombreUsuario.trim().length === 0 || password.trim().length === 0) {
             throw new Error("Los datos no son correctos");
         }
 
-        fetch("https://censo.develotion.com/login.php", {
+        // Hacer la llamada a la API para el login
+        fetch(API_LOGIN_ENDPOINT, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -97,34 +106,31 @@ function IniciarSesion() {
                 "password": password
             })
         })
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                return Promise.reject(response);
-            }
-        })
-        .then(function (data) {
-            // Si llegamos aquí, no hubo errores en el servidor y se obtuvo la respuesta correctamente
-            //hayUsuarioLogueado = true;
-            localStorage.setItem("token", data.apiKey);
-            localStorage.setItem("hayUsuarioLogueado", true);
-            Inicio(true);
-            OcultarBotones(false);
-        })
-        .catch(function (error) {
-            // Imprimir el error en la consola para obtener más detalles
-            console.error("Error en fetch:", error);
-            // Devolver el error para que la siguiente cadena de promesas lo maneje
-            return error.json();
-        })
-        .then(function (data) {
-            // Si llegamos aquí, estamos manejando un error del servidor
-            if (data !== undefined) {
-                // Mostrar el mensaje de error en el HTML
-                document.querySelector("#errorMessage").innerHTML = data.error;
-            }
-        });
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return Promise.reject(response);
+                }
+            })
+            .then((data) => {
+                // Si el login fue exitoso, se obtiene el token del usuario
+                token = data.apiKey;
+                // Se guarda el token y el estado de sesión en el localStorage
+                localStorage.setItem("token", token);
+                localStorage.setItem("hayUsuarioLogueado", true);
+                // Mostrar la sección de usuario logueado
+                Inicio(false);
+                // Ocultar los botones de inicio de sesión y registro
+                OcultarBotones(false);
+            })
+            .catch(handleApiError) // Utilizar la función para manejar errores de la API
+            .then((data) => {
+                // Si hay error en la respuesta de la API, mostrar el mensaje de error
+                if (data !== undefined) {
+                    document.querySelector("#errorMessage").innerHTML = data.error;
+                }
+            });
     } catch (error) {
         // Si hay errores en el bloque try-catch, mostrarlos en la consola para depuración
         console.error("Error en try-catch:", error);
@@ -132,19 +138,18 @@ function IniciarSesion() {
     }
 }
 
-
-
+// Función para cerrar sesión
 function CerrarSesion() {
-    localStorage.setItem("hayUsuarioLogueado",false);
-    document.querySelector("#btnCerrarSesion").style.display = "none";
-    Inicio(true);
-    console.log(localStorage.getItem("token"));
+    // Limpiar el token y el estado de sesión del localStorage
     localStorage.removeItem("token");
-    console.log(localStorage.getItem("token"));
+    localStorage.setItem("hayUsuarioLogueado", false);
+    // Mostrar la sección de inicio de sesión
+    Inicio(true);
+    // Limpiar los campos de usuario y contraseña
     LimpiarCampos();
 }
 
-
+// Función para registrar un nuevo usuario
 function Registro() {
     let nombreUsuario = document.querySelector("#usuario").value;
     let password = document.querySelector("#passRegistro").value;
@@ -154,64 +159,69 @@ function Registro() {
             throw new Error("El nombre de usuario es requerido");
         }
         if (password.trim().length === 0) {
-            throw new Error("La passwrod es requerida");
+            throw new Error("La password es requerida");
         }
 
-        fetch("https://censo.develotion.com/usuarios.php",
-            {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json"
-                },
-                body: JSON.stringify({
-                    "usuario": nombreUsuario,
-                    "password": password
-                })
+        // Hacer la llamada a la API para el registro
+        fetch(API_USUARIOS_ENDPOINT, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                "usuario": nombreUsuario,
+                "password": password
             })
-            .then(function (response) {
+        })
+            .then((response) => {
                 if (response.ok) {
-                    // return response.json();
+                    // Mostrar mensaje de registro exitoso
                     document.querySelector("#errorMessageRegistro").innerHTML = "Registro exitoso";
                     LimpiarCampos();
-                }
-                else if(response.status === 409){
-                    document.querySelector("#errorMessageRegistro").innerHTML = "El usuario ya esta registrado";
+                } else if (response.status === 409) {
+                    // Mostrar mensaje de usuario ya registrado
+                    document.querySelector("#errorMessageRegistro").innerHTML = "El usuario ya está registrado";
                     LimpiarCampos();
                     return Promise.reject(response);
-                }
-                else {
+                } else {
                     return Promise.reject(response);
                 }
             })
-            .then(function (data) {
-                // Si llegamos aquí, no hubo errores en el servidor y se obtuvo la respuesta correctamente
-                localStorage.setItem("hayUsuarioLogueado",true);
-                //hayUsuarioLogueado = true;
-                console.log(data.apiKey);
-                localStorage.setItem("token", data.apiKey);
-                Inicio(true);
+            .then((data) => {
+                // Si el registro fue exitoso, obtener el token del usuario
+                token = data.apiKey;
+                // Guardar el token y el estado de sesión en el localStorage
+                localStorage.setItem("token", token);
+                localStorage.setItem("hayUsuarioLogueado", true);
+                // Mostrar la sección de usuario logueado
+                Inicio(false);
+                // Ocultar los botones de inicio de sesión y registro
                 OcultarBotones(false);
             })
-            .catch(function (error) {
-                // Imprimir el error en la consola para obtener más detalles
-                console.error("Error en fetch:", error);
-                // Devolver el error para que la siguiente cadena de promesas lo maneje
-                return error.json();
-            })
-            .then(function (data) {
-
+            .catch(handleApiError) // Utilizar la función para manejar errores de la API
+            .then((data) => {
+                // Si hay error en la respuesta de la API, mostrar el mensaje de error
                 if (data !== undefined) {
                     document.querySelector("#errorMessage").innerHTML = data.error;
                 }
-            })
-
-        } catch (Error) {
-            document.querySelector("#errorMessage").innerHTML = Error.message;
-    
-        }
+            });
+    } catch (error) {
+        // Si hay errores en el bloque try-catch, mostrarlos en la consola para depuración
+        console.error("Error en try-catch:", error);
+        document.querySelector("#errorMessage").innerHTML = error.message;
     }
+}
 
     function LimpiarCampos() {
         document.querySelector("#usuario").value = "";
         document.querySelector("#passRegistro").value = "";
     }
+// Función para manejar los errores de la API
+function handleApiError(error) {
+    console.error("Error en fetch:", error);
+    return error.json().then((data) => {
+        if (data !== undefined) {
+            document.querySelector("#errorMessage").innerHTML = data.error;
+        }
+    });
+}
